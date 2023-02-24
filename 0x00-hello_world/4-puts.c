b6e7981dd381da38204106a5cf3bc7fdc599e760
MakedaF <makedareda@gmail.com>
2023-02-24 03:12:56 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,11 @@
  * main- print "Programming is like building 
  * a multilingual puzzle, followed by a newline.
  *
- * Return: Always 0 (Sucess).
+ * Return: Always 0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
