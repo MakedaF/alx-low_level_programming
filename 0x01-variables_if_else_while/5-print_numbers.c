1b47373d1666bae5cbd960d6281b2de3b5c86ee6
MakedaF <makedareda@gmail.com>
2023-02-26 07:33:19 -0800

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - prints digit numbers
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main (void)
+{
+	int i = '0';
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
