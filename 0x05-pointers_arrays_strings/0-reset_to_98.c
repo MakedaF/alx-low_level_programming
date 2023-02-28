cabe055c32d312e7fac16b32a0e64af6639024aa
MakedaF <makedareda@gmail.com>
2023-02-28 11:43:07 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,9 @@
 /**
  * reset_to_98 - takes a pointer to an int and updates the 
  * value it points to to 98
+ *
  * @n: an integer input pointer
+ *
  * Return: nothing
  */
 void reset_to_98(int *n)
