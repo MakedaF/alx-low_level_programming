d1532113238231357ac58b97db06f7c4cbbcfc5b
MakedaF <makedareda@gmail.com>
2023-02-28 03:15:45 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - test if integer is positive or negative
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int 1;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
