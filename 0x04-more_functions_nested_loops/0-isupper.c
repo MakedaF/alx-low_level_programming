825227a62279d9eb3f9f81bf73f0b1d268be777c
MakedaF <makedareda@gmail.com>
2023-02-28 09:14:36 -0800

    back

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - check if letter is uppercase
+ * @x: the number to be checked
+ * Return: 1 for uppercase or 0 otherwise
+ */
+
+int _isupper(int x)
+{
+	if (x >= 65 && <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
