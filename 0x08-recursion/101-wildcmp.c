3d8e93c607aabe715d5f13dc070a4730ec449821
MakedaF <makedareda@gmail.com>
2023-03-08 10:06:45 -0800

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -4,7 +4,7 @@
  * wildcmp - compares two strings
  * @s1: string 1
  * @s2: string 2
- * Return; 1 if identical otherwise 0
+ * Return: 1 if identical otherwise 0
  */
 int wildcmp(char *s1, char *s2)
 {
