c7c9358694d0269f3e66fac1fc0413b1b9839e6c
MakedaF <makedareda@gmail.com>
2024-05-20 06:17:08 -0700

    edit

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,5 @@
 #include "search_algos.h"
-
+#include <stdio.h>
 /**
  * linear_search: linear search for value in an array
  *
@@ -15,6 +15,7 @@ int linear_search(int *array, size_t size, int value)
 	}
 	for (i = 0; i < size; i++)
 	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{ return (int)i;
 		}
