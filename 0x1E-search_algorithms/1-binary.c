c4ffefd7cb9b19fde787f38a14e2570047a177e8
MakedaF <makedareda@gmail.com>
2024-05-21 02:41:06 -0700

    1-binary

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,44 @@
+#include "search_algos.h"
+#include <stdio.h>
+
+/**
+ * binary_search: performs binary search on array
+ * *array: pointer to first element in array
+ * size: size of array
+ * value: the target
+ *
+ * Return: value if success, else -1
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t l = 0;
+	size_t r = size - 1;
+	size_t i = l;
+
+	if (array == NULL)
+	{ return -1;
+	}
+	while (l <= r)
+	{
+		size_t mid = (l + r) / 2;
+
+		printf("Searching in array: ");
+		for (i = l; i <= r; i++)
+		{
+			printf("%d ", array[i]);
+		}
+		printf("\n");
+		if (array[mid] < value)
+		{
+			l = mid + 1;
+		}
+		else if (array[mid] > value)
+		{
+			r = mid - 1;
+		}
+		else
+		{ return (int)mid;
+		}
+	} return -1;
+}
