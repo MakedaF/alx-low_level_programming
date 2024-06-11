c2463a1f211894391b3d43303238412d7691060c
MakedaF <makedareda@gmail.com>
2024-06-11 01:48:02 -0700

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,7 +7,7 @@
  *
  * Return: value of (base ^ power)
  */
-void print_binary(unsigned long int n)
+unsigned long int _pow(unsigned int base, unsigned int power)
 {
 	unsigned long int num;
 	unsigned int a;
